c41b38a0063e1d07a46c84dfe4a2859187fa394a
ArchyDev <sebastianglez05@gmail.com>
2022-08-04 21:37:39 -0500

    Commit - Week 1

---
+++ app/src/main/java/com/example/weeklychallenges/Week_1/ANAGRAMA.kt
@@ -0,0 +1,10 @@
+package com.example.weeklychallenges.Week_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ANAGRAMA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
