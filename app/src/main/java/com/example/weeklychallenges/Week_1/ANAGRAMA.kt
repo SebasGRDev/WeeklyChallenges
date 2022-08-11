3559d8670afe2cfbcc2aa46132d856c6fa39419f
ArchyDev <sebastianglez05@gmail.com>
2022-08-10 21:50:55 -0500

    Commit - Week 1

--- app/src/main/java/com/example/weeklychallenges/Week_1/ANAGRAMA.kt
+++ app/src/main/java/com/example/weeklychallenges/Week_1/ANAGRAMA.kt
@@ -25,5 +25,12 @@ import android.os.Bundle
 class ANAGRAMA : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        isAnagram("Sergio", "Riesgo")
     }
 }
+    fun isAnagram(wordOne: String, wordTwo: String): Boolean {
+        if (wordOne.lowercase() == wordTwo.lowercase()){
+            return false
+        }
+        return wordOne.lowercase().toCharArray().sortedArray().contentEquals(wordTwo.lowercase().toCharArray().sortedArray())
+    }
